a510cc931d489ef34847ff70e9d1f41bbc495b6f
Guada-Maricchiolo <guada.maricchiolo@gmail.com>
2024-09-26 00:03:49 -0300

    Agregamos ejercicio 3, revisar como quedaron las clases

---
+++ Ejercicio3.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int h;
+        int k;
+        System.out.println("Ingrese un valor entero mayor o igual a 3");
+        int X = scanner.nextInt();
+        if (X <= 3) {
+            System.out.println("El número ingresado no cumple con el parámetro, intente nuevamente");
+        }
+        int[][] arrayCase3 = new int[X][X];
+        System.out.println("Ingrese los valores del array");
+        for ( h = 0; h < X; h++) {
+            for ( k = 0; k < X; k++) {
+                System.out.println("Elemento [" + h + "][" + k + "]: ");
+                arrayCase3[h][k]= scanner.nextInt();
+            }
+        }
+        System.out.println("Array ingresado:");
+        for (h = 0; h <X; h++) {
+            for (k = 0; k <X; k++) {
+                System.out.println(arrayCase3[h][k] + " ");
+            }
+            System.out.println(); //esto es una nueva línea para cada fila
+        }
+
+        int[] sumaColumnas = new int[X];
+        //para sumar los valores de cada columna:
+        for ( h = 0; h <X; h++) {
+            int suma = 0;
+            for (k = 0; k<arrayCase3.length; k++) {
+                suma += arrayCase3[k][h];
+            }
+            sumaColumnas[h]=suma; //asignamos la suma al nuevo array
+        }
+        System.out.println("Suma de cada columna:");
+        for (k = 0; k<X; k++) {
+            System.out.println("Columna " + k + ": " + sumaColumnas[k]);
+        }
+        int sumaTotal = 0;
+        for (int i = 0; i <sumaColumnas.length ; i++) {
+            sumaTotal+=sumaColumnas[i];
+        }
+        System.out.println("Suma total de todas las columnas: " + sumaTotal);
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -20,18 +20,80 @@ public class Main {
                             case 1:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 1:");
-                                Ejercicio1.main(args);
+                                String[][] ciudadesPorPais = new String[4][4];
+                                System.out.println("Ingrese cuatro países y tres ciudades de cada país:");
+                                for (int i = 0; i < 4; i++) {
+                                    System.out.println("PAÍS " + (i + 1) + ":");
+                                    ciudadesPorPais[i][0] = sc.nextLine();
+                                    for (int j = 1; j < 4; j++) {
+                                        System.out.println("Ciudad " + j + ":");
+                                        ciudadesPorPais[i][j] = sc.nextLine();
+                                    }
+                                    System.out.println();
+                                }
+                                System.out.println("Ciudades por país:");
+                                for (int i = 0; i < 4; i++) {
+                                    System.out.print("País: " + ciudadesPorPais[i][0] + "\t\tCiudades:");
+                                    for (int j = 1; j < 4; j++) {
+                                        System.out.print("\t" + ciudadesPorPais[i][j]);
+                                    }
+                                    System.out.println();
+                                }
                                 break;
+
                             case 2:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 2:");
 
                                 break;
+
                             case 3:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 3:");
+                                Scanner scanner = new Scanner(System.in);
+                                int h;
+                                int k;
+                                System.out.println("Ingrese un valor entero mayor o igual a 3");
+                                int X = scanner.nextInt();
+                                if (X <= 3) {
+                                    System.out.println("El número ingresado no cumple con el parámetro, intente nuevamente");
+                                }
+                                int[][] arrayCase3 = new int[X][X];
+                                System.out.println("Ingrese los valores del array");
+                                for ( h = 0; h < X; h++) {
+                                    for ( k = 0; k < X; k++) {
+                                        System.out.println("Elemento [" + h + "][" + k + "]: ");
+                                        arrayCase3[h][k]= scanner.nextInt();
+                                    }
+                                }
+                                System.out.println("Array ingresado:");
+                                for (h = 0; h <X; h++) {
+                                    for (k = 0; k <X; k++) {
+                                        System.out.println(arrayCase3[h][k] + " ");
+                                    }
+                                    System.out.println(); //esto es una nueva línea para cada fila
+                                }
 
+                                int[] sumaColumnas = new int[X];
+                                //para sumar los valores de cada columna:
+                                for ( h = 0; h <X; h++) {
+                                    int suma = 0;
+                                    for (k = 0; k<arrayCase3.length; k++) {
+                                        suma += arrayCase3[k][h];
+                                    }
+                                    sumaColumnas[h]=suma; //asignamos la suma al nuevo array
+                                }
+                                System.out.println("Suma de cada columna:");
+                                for (k = 0; k<X; k++) {
+                                    System.out.println("Columna " + k + ": " + sumaColumnas[k]);
+                                }
+                                int sumaTotal = 0;
+                                for (int i = 0; i <sumaColumnas.length ; i++) {
+                                    sumaTotal+=sumaColumnas[i];
+                                }
+                                System.out.println("Suma total de todas las columnas: " + sumaTotal);
                                 break;
+
                             case 4:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 4:");
