83ee9ed0df64f0e9e45cd3ca8c8a720c9d68001a
barrera-miguel <miguelbo138@gmail.com>
2024-09-26 00:48:58 -0300

    Borrar código Ejercicio1 del Main

--- src/Main.java
+++ src/Main.java
@@ -20,27 +20,8 @@ public class Main {
                             case 1:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 1:");
-                                String[][] ciudadesPorPais = new String[4][4];
-                                System.out.println("Ingrese cuatro países y tres ciudades de cada país:");
-                                for (int i = 0; i < 4; i++) {
-                                    System.out.println("PAÍS " + (i + 1) + ":");
-                                    ciudadesPorPais[i][0] = sc.nextLine();
-                                    for (int j = 1; j < 4; j++) {
-                                        System.out.println("Ciudad " + j + ":");
-                                        ciudadesPorPais[i][j] = sc.nextLine();
-                                    }
-                                    System.out.println();
-                                }
-                                System.out.println("Ciudades por país:");
-                                for (int i = 0; i < 4; i++) {
-                                    System.out.print("País: " + ciudadesPorPais[i][0] + "\t\tCiudades:");
-                                    for (int j = 1; j < 4; j++) {
-                                        System.out.print("\t" + ciudadesPorPais[i][j]);
-                                    }
-                                    System.out.println();
-                                }
+                                Ejercicio1.main(args);
                                 break;
-
                             case 2:
                                 System.out.println("-------------------------------------");
                                 System.out.println("Ejercicio 2:");
